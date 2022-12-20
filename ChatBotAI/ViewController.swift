7134beb9149665fe0b97e3b2b3a0a1fb2dd3f5ca
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2022-12-20 15:09:58 +0100

    bot response

--- ChatBotAI/ViewController.swift
+++ ChatBotAI/ViewController.swift
@@ -8,12 +8,83 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let field: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Type here..."
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.returnKeyType = .done
+        return tf
+    }()
+    
+    private var models = [String]()
+    
+    private let table: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupSubviews()
+        setupConstraints()
     }
 
+    private func setupSubviews() {
+        view.addSubview(field)
+        view.addSubview(table)
+        field.delegate = self
+        table.delegate = self
+        table.dataSource = self
+    }
+    
+    private func setupConstraints() {
+        let constraints = [
+            field.heightAnchor.constraint(equalToConstant: 60),
+            field.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10),
+            field.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            field.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor),
+            
+            table.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            table.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            table.bottomAnchor.constraint(equalTo: field.topAnchor),
+            table.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row]
+        cell.textLabel?.numberOfLines = 0
+        return cell
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let text = textField.text, !text.isEmpty {
+            APICaller.shared.getResponse(input: text) {[weak self] result in
+                switch result {
+                case .success(let output):
+                    self?.models.append(output)
+                    DispatchQueue.main.async {
+                        self?.table.reloadData()
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
+        return true
+    }
+    
+}
