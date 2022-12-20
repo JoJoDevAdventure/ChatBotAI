ad338cf8048fdfae5ba02192c2ec5ba8fb561a74
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2022-12-20 13:53:46 +0100

    Initial Commit

---
+++ ChatBotAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatBotAI
+//
+//  Created by Youssef Bhl on 20/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
